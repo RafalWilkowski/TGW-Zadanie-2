4b85e86736295cf0aa2eed4e79f04ef8b80d751f
Hubert Welenc <hubert.welenc@op.pl>
2020-02-16 00:45:33 +0100

    Lepiej nie mergować do mastera! Grafika od Rafała i realtimeowe lampy. Duży spadek wydajności.

---
+++ Zadanie2/Assets/Scripts/Test/LampAsynchronizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampAsynchronizer : MonoBehaviour
+{
+	Animator[] anims;
+	int asyncPropertyHash, speedPropertyHash;
+	[SerializeField] string asyncProperty = "async";
+	[SerializeField] string speedProperty = "speed";
+    // Start is called before the first frame update
+    void Awake()
+    {
+		anims = GetComponentsInChildren<Animator>();
+		asyncPropertyHash = Animator.StringToHash(asyncProperty);
+		speedPropertyHash = Animator.StringToHash(speedProperty);
+		foreach (Animator anim in anims)
+		{
+			anim.SetFloat(asyncPropertyHash, Random.value);
+			anim.SetFloat(speedProperty, Random.Range(0.7f, 1.1f));
+		}
+		enabled = false;
+    }
+}
