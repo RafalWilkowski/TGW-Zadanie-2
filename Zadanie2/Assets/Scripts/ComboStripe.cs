f6b7838598a2e21401ad9ac8565fb84c99cbd9a0
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-19 15:33:03 +0100

    Add combo time stripe

---
+++ Zadanie2/Assets/Scripts/ComboStripe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ComboStripe : MonoBehaviour
+{
+    public Action OnTimeout;
+
+    [SerializeField]
+    private float _firstTimeToAdd;
+    [SerializeField]
+    private float _nextTimetoAdd;
+    
+    private Slider _slider;
+    private bool _timeRunning = false;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timeRunning)
+        {
+            if (_slider.value > 0)
+            {
+                _slider.value -= Time.deltaTime;
+            }
+            else
+            {
+                _timeRunning = false;
+                OnTimeout?.Invoke();
+            }
+        }       
+    }
+
+    public void UpdateTime()
+    {
+        if (!_timeRunning)
+        {
+            _slider.value += 2;
+        }
+        else
+        {
+            _slider.value += 1;
+        }
+        _timeRunning = true;
+    }
+}
--- Zadanie2/Assets/Scripts/Conteiner.cs
+++ Zadanie2/Assets/Scripts/Conteiner.cs
@@ -6,6 +6,7 @@ using System;
 public class Conteiner : MonoBehaviour
 {
     public Action<ObjectColor> OnColorMatch;
+    public Action OnColorMatched;
 
     [field: SerializeField]
     public ObjectColor ObjectColor { get; private set; }
