84e77d82b066b5de3024292fdb96f61846ab49b9
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-21 19:05:08 +0100

    Add bomb body and fuse

---
+++ Zadanie2/Assets/Scripts/Fuse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Fuse : MonoBehaviour
+{
+    public Action OnExplode;
+
+    private float _timeToExplode = 5f;
+    // Start is called before the first frame update
+    public void Init(float time)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeToExplode = -Time.deltaTime;
+        if (_timeToExplode <= 0) OnExplode?.Invoke();
+    }
+}
