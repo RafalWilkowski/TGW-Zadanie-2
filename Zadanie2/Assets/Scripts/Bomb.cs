c550cff2c1c4a2178811c4433ebc6f80e783a248
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-21 19:05:08 +0100

    Add bomb body and fuse
    (cherry picked from commit 84e77d82b066b5de3024292fdb96f61846ab49b9)

---
+++ Zadanie2/Assets/Scripts/Bomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour , IInteractable
+{
+    private Fuse _fuse;
+    private void Start()
+    {
+        _fuse = transform.GetChild(0).GetComponentInChildren<Fuse>();
+        if (_fuse == null) Debug.Log("fusenotfound");
+    }
+    public void Interact(int touchID)
+    {
+        _fuse.gameObject.SetActive(false);
+    }
+
+    public void Explode()
+    {
+
+    }
+}
