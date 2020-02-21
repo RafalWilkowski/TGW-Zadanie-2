b407f13ba9315bac0c0e50595c8ac9132496abd3
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-21 18:20:51 +0100

    Fix problem with very fast swipe

--- Zadanie2/Assets/Scripts/Gem.cs
+++ Zadanie2/Assets/Scripts/Gem.cs
@@ -98,6 +98,7 @@ public class Gem : MonoBehaviour , IInteractable
 
     private void OnFingerPositionChanged(Vector2 position)
     {
+        rb2D.position = position;
         rb2D.velocity = Vector2.zero;
         rb2D.freezeRotation = true;
         transform.position = new Vector3(position.x, position.y, transform.position.z);
