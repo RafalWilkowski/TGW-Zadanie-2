aa9bea5430170694a152cd2df975707744c36852
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-24 21:49:24 +0100

    Add odd system to breakable stone

--- Zadanie2/Assets/Scripts/GemSpawner.cs
+++ Zadanie2/Assets/Scripts/GemSpawner.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class GemSpawner : MonoBehaviour
 {
     public static GemSpawner Instance;
 
     [SerializeField]
-    private float _trashSpawnRate = 1f;
+    private float _gemSpawnRate = 1f;
     private float _spawnCooldown = 0;
 
+    [SerializeField]
+    private float[] _wagiPrawdopodobienstwa;
+    private float[] _rozKumultatywny;
     private int _specialPackage = 0;
 
     [SerializeField]
@@ -24,6 +28,9 @@ public class GemSpawner : MonoBehaviour
     }
     private void Start()
     {
+        // preparing gem container (breakable stone) odds
+        _rozKumultatywny = new float[_wagiPrawdopodobienstwa.Length];
+        StoneBreakOdds();
         //copying gemsprites array to dictionary
         foreach(GemSprite gemSprite in gemSprites)
         {
@@ -35,20 +42,21 @@ public class GemSpawner : MonoBehaviour
     {
         if(_spawnCooldown <= Time.time)
         {
-            _spawnCooldown = Time.time + _trashSpawnRate;
-            _specialPackage += UnityEngine.Random.Range(0, 20);
+            _spawnCooldown = Time.time + _gemSpawnRate;
+            _specialPackage += UnityEngine.Random.Range(20, 50);
             float randY = UnityEngine.Random.Range(-1, 1);
             Vector3 spawnPoint = new Vector3(transform.position.x, transform.position.y - randY, -1.5f);
 
             if (_specialPackage > 100)
             {
-                _specialPackage = 0;             
-                var gemConteiner = StonePool.Instance.GetObjectFromPool();
-                gemConteiner.transform.position = spawnPoint;
+              _specialPackage = 0;             
+              var gemConteiner = StonePool.Instance.GetObjectFromPool();
+              gemConteiner.transform.position = spawnPoint;
+              gemConteiner.OnBreak += StoneBreak;
             }
             else
             {
-                SpawnGem(spawnPoint);
+              SpawnGem(spawnPoint);
             }
         }
     }
@@ -67,13 +75,46 @@ public class GemSpawner : MonoBehaviour
     }
     public void StoneBreak(Vector3 position)
     {
-        /*int quantity = UnityEngine.Random.Range(0, _prefabs.Length + 1);
-        print("Ilość:" + quantity.ToString());
+        int quantity = 0;
+        float randomFloat = UnityEngine.Random.Range(0f, 1f);
+
+        for(int i = 0; i < _rozKumultatywny.Length; i++)
+        {
+            float compareFloat = _rozKumultatywny[i];
+            if (randomFloat < compareFloat)
+            {
+                quantity = i;                
+                break;
+            }               
+        }
+        
+        Debug.Log(randomFloat + " : : " + quantity);
+        
         for (int i = 0; i < quantity; i++)
         {
-            int rand = UnityEngine.Random.Range(0, _prefabs.Length-2);
-            Instantiate(_prefabs[rand], position, Quaternion.identity);
-        }*/
+            SpawnGem(position);
+        }
+    }
+
+    public void StoneBreakOdds()
+    {
+        float totalWeight = _wagiPrawdopodobienstwa.Sum();
+        List<float> rozkladPrawd = new List<float>();
+        int lenght = _wagiPrawdopodobienstwa.Length;
+
+        // rozkład pradwopodobienstwa
+        for (int i = 0; i < lenght; i++)
+        {
+            float someValue = _wagiPrawdopodobienstwa[i] / totalWeight;
+            rozkladPrawd.Add(someValue);
+        }
+        // rozklad kumultatywny
+        for(int i = lenght - 1 ; i >= 0; --i)
+        {           
+            float suma = rozkladPrawd.Sum();
+            _rozKumultatywny[i] = suma;
+            rozkladPrawd.RemoveAt(rozkladPrawd.Count - 1);                     
+        }
     }
 }
 
