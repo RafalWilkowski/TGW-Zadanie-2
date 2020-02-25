3d787166c785efac80b460afc2540cfa3a1cb8e7
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-25 20:19:44 +0100

    Refactor code Organze breakable stone system

--- Zadanie2/Assets/Scripts/GemSpawner.cs
+++ Zadanie2/Assets/Scripts/GemSpawner.cs
@@ -12,9 +12,6 @@ public class GemSpawner : MonoBehaviour
     private float _gemSpawnRate = 1f;
     private float _spawnCooldown = 0;
 
-    [SerializeField]
-    private float[] _wagiPrawdopodobienstwa;
-    private float[] _rozKumultatywny;
     private int _specialPackage = 0;
 
     [SerializeField]
@@ -28,9 +25,6 @@ public class GemSpawner : MonoBehaviour
     }
     private void Start()
     {
-        // preparing gem container (breakable stone) odds
-        _rozKumultatywny = new float[_wagiPrawdopodobienstwa.Length];
-        StoneBreakOdds();
         //copying gemsprites array to dictionary
         foreach(GemSprite gemSprite in gemSprites)
         {
@@ -52,69 +46,30 @@ public class GemSpawner : MonoBehaviour
               _specialPackage = 0;             
               var gemConteiner = StonePool.Instance.GetObjectFromPool();
               gemConteiner.transform.position = spawnPoint;
-              if(gemConteiner.OnBreak == null) gemConteiner.OnBreak += StoneBreak;
+              if(gemConteiner.OnBreak == null) gemConteiner.OnBreak += SpawnGems;
             }
             else
             {
-              SpawnGem(spawnPoint);
+              SpawnGems(1, spawnPoint);
             }
         }
     }
 
-    private void SpawnGem(Vector3 spawnPoint)
+    private void SpawnGems(int numberOfGems, Vector3 spawnPoint)
     {
         //randomize gems colors
-        Array colorsArray = Enum.GetValues(typeof(ObjectColor));       
-        int rand = UnityEngine.Random.Range(1, colorsArray.Length);
-        ObjectColor colorType = (ObjectColor)colorsArray.GetValue(rand);
-        
-        // get gem from pool and initalize it
-        var gem = GemPool.Instance.GetObjectFromPool();
-        Sprite spriteType = gemSpritesDic[(int)colorType];
-        gem.Init(colorType, spawnPoint,spriteType);
-    }
-    public void StoneBreak(Vector3 position)
-    {
-        int quantity = 0;
-        float randomFloat = UnityEngine.Random.Range(0f, 1f);
-
-        for(int i = 0; i < _rozKumultatywny.Length; i++)
+        Array colorsArray = Enum.GetValues(typeof(ObjectColor));    
+        for(int i = 0; i < numberOfGems; i++)
         {
-            float compareFloat = _rozKumultatywny[i];
-            if (randomFloat < compareFloat)
-            {
-                quantity = i;                
-                break;
-            }               
+            int rand = UnityEngine.Random.Range(1, colorsArray.Length);
+            ObjectColor colorType = (ObjectColor)colorsArray.GetValue(rand);
+
+            // get gem from pool and initalize it
+            var gem = GemPool.Instance.GetObjectFromPool();
+            Sprite spriteType = gemSpritesDic[(int)colorType];
+            gem.Init(colorType, spawnPoint, spriteType);
         }
         
-        Debug.Log(randomFloat + " : : " + quantity);
-        
-        for (int i = 0; i < quantity; i++)
-        {
-            SpawnGem(position);
-        }
-    }
-
-    public void StoneBreakOdds()
-    {
-        float totalWeight = _wagiPrawdopodobienstwa.Sum();
-        List<float> rozkladPrawd = new List<float>();
-        int lenght = _wagiPrawdopodobienstwa.Length;
-
-        // rozkład pradwopodobienstwa
-        for (int i = 0; i < lenght; i++)
-        {
-            float someValue = _wagiPrawdopodobienstwa[i] / totalWeight;
-            rozkladPrawd.Add(someValue);
-        }
-        // rozklad kumultatywny
-        for(int i = lenght - 1 ; i >= 0; --i)
-        {           
-            float suma = rozkladPrawd.Sum();
-            _rozKumultatywny[i] = suma;
-            rozkladPrawd.RemoveAt(rozkladPrawd.Count - 1);                     
-        }
     }
 }
 
