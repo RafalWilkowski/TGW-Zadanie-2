3338510d58c2b878ebd1cfe0e0afd24c085d3208
RafalWilkowski <kaizen.avc@gmail.com>
2020-02-26 15:10:40 +0100

    Refactor bomm trigger

--- Zadanie2/Assets/Scripts/BombTrigger.cs
+++ Zadanie2/Assets/Scripts/BombTrigger.cs
@@ -9,33 +9,15 @@ public class BombTrigger : MonoBehaviour , IInteractable
     [SerializeField]
     private float _triggerDelayTime = 2f;
 
-    private CapsuleCollider2D _capsuleCollider2D;
-
-    private void Start()
-    {
-        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
-    }
-
-    private void OnEnable()
-    {
-        if(_capsuleCollider2D != null) _capsuleCollider2D.enabled = false;
-        StartCoroutine(TriggerDelay());
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Gem gem = collision.GetComponent<Gem>();
-        if(gem)
+        if(gem && collision.isTrigger)
         {
             if(_exlosionPrefab != null) Instantiate(_exlosionPrefab, transform.position, Quaternion.identity);
             Bomb bomb = GetComponentInParent<Bomb>();
             DynamitePool.Instance.ReturnToPool(bomb);
         }       
-    }
-
-    private IEnumerator TriggerDelay()
-    {
-        yield return new WaitForSeconds(_triggerDelayTime);
-        _capsuleCollider2D.enabled = true;
     }
 
     public void Interact(int touchID)
