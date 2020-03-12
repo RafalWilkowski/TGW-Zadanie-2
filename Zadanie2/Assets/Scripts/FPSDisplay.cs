cab281b125843e6cab577d8b64aab1de5c1458ce
RafalWilkowski <kaizen.avc@gmail.com>
2020-03-12 20:54:07 +0100

    Change images quality settings , Add FPS counter

---
+++ Zadanie2/Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+    float _fps = 0.0f;
+    Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+    void Update()
+    {
+        _fps += (Time.unscaledDeltaTime - _fps) * 0.1f;
+        int fps = Mathf.RoundToInt( 1.0f / this._fps);      
+        _text.text = fps + " fps";
+    }
+
+}
