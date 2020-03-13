a1b982d20dab434ba0340bd6d1fb70de1cb4400c
RafalWilkowski <kaizen.avc@gmail.com>
2020-03-13 20:06:13 +0100

    Add dynamites FX and SFX effects

--- Zadanie2/Assets/Scripts/Bomb.cs
+++ Zadanie2/Assets/Scripts/Bomb.cs
@@ -6,6 +6,9 @@ public class Bomb : MonoBehaviour , IInteractable
 {
     private Fuse _fuse;
     private BombTrigger _bombTrigger;
+    [SerializeField]
+    private AudioClip[] _puttingOutFuse;
+    private AudioSource _audio;
 
 	public delegate void BombDelegate(Bomb bomb);
 	public event BombDelegate OnBombDisabled;
@@ -15,19 +18,24 @@ public class Bomb : MonoBehaviour , IInteractable
         if(_fuse != null) _fuse.gameObject.SetActive(true);
         if (_bombTrigger != null) _bombTrigger.gameObject.SetActive(true);
     }
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
     private void Start()
     {
         _fuse = transform.GetChild(0).GetComponentInChildren<Fuse>();
-        if (_fuse == null) Debug.Log("fusenotfound");
-        _bombTrigger = GetComponentInChildren<BombTrigger>();
-        
+        _bombTrigger = GetComponentInChildren<BombTrigger>();       
     }
+
     public void Interact(int touchID)
     {
         if (_fuse.isActiveAndEnabled)
         {
             _fuse.gameObject.SetActive(false);
             _bombTrigger.gameObject.SetActive(false);
+            int randomClip = Random.Range(0, _puttingOutFuse.Length);
+            _audio.PlayOneShot(_puttingOutFuse[randomClip]);
         }       
     }
 
