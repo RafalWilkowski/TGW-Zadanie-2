d901274f929e01e338aac6df7c43699c3172aeda
Hubert Welenc <hubert.welenc@op.pl>
2020-03-21 11:46:19 +0100

    Przygotowano tutorial i podpięto go pod odtwarzacz w grze.
    Zauważyłem, że wokół niektórych elementów pojawia się biały outline. Dodałem materiał, który pozwala na zrobienie odcięcia alphy, aby zredukować ten problem. Nie wrzucałem wszędzie, bo nie wiem, jak to się odbije na wydajności.
    
    Dodałem utility do robienia screenów w rozdzielczości HD.

---
+++ Zadanie2/Assets/Scripts/Test/CaptureScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CaptureScreen : MonoBehaviour
+{
+	[SerializeField] bool shootOnStart;
+	[SerializeField] float shootInterval = 1f;
+	[SerializeField] float delay = 0;
+	int screenshotNumber;
+	float startTime, lastScreenshotTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (shootOnStart)
+		{
+			ScreenCapture.CaptureScreenshot("D:/ScreenShot.png");
+			Debug.Log("Screenshot taken!");
+
+		}
+		startTime = lastScreenshotTime = Time.time;
+	}
+	private void Update()
+	{
+		if (Time.time - startTime < delay) return;
+		if (Time.time - lastScreenshotTime > shootInterval)
+		{
+			string screenshotName = string.Format("D:/ScreenShot{0:0000}.png", screenshotNumber);
+			ScreenCapture.CaptureScreenshot(screenshotName);
+			Debug.LogFormat("Screenshot {0} taken!", screenshotName);
+			screenshotNumber++;
+			lastScreenshotTime = Time.time;
+		}
+	}
+}
+ 
